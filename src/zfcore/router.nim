86b45180c1db6ee61eaa1c28c1d97accbaecebb4
Amru Rosyada <amru.rosyada@gmail.com>
2020-03-30 08:45:58 +0700

    set compression level to -9 aka best

--- src/zfcore/router.nim
+++ src/zfcore/router.nim
@@ -319,7 +319,7 @@ proc tryCompress(
 
     if self.isContentShouldCompress(contentType):
         var (output, exitCode) = execCmdEx(
-            &"gzip -1 -q --to-stdout {filePath}")
+            &"gzip -9 --to-stdout {filePath}")
 
         if exitCode == 0:
             return (
