f2f6c2c7ce9e646f0343c8fbf5c8ab90f78e0d10
Amru Rosyada <amru.rosyada@gmail.com>
2020-04-11 02:44:42 +0700

    test compression

--- src/zfcore/router.nim
+++ src/zfcore/router.nim
@@ -322,7 +322,7 @@ proc tryCompress(
 
     if self.isContentShouldCompress(contentType):
         var (output, exitCode) = execCmdEx(
-            &"gzip -3 --to-stdout {filePath}")
+            &"gzip -2 --to-stdout {filePath}")
 
         if exitCode == 0:
             return (
