0804d10c1d25ea7f03ef8d5a92b0b182c104b9be
Amru Rosyada <amru.rosyada@gmail.com>
2021-02-22 13:20:12 +0700

    add status code into ApiMsg

--- src/apimsg.nim
+++ src/apimsg.nim
@@ -7,23 +7,30 @@
   Git: https://github.com/zendbit
 ]#
 
-import json
-export json
+import json, httpcore
+export json, httpcore
 
 type
   ApiMsg* = ref object
+    status*: HttpCode
     success*: bool
     error*: JsonNode
     data*: JsonNode
 
 proc `%`*(self: ApiMsg): JsonNode =
   result = %*{}
+  result["status"] = % $self.status
   result["success"] = %self.success
   result["error"] = self.error
   result["data"] = self.data
 
 proc newApiMsg*(
   success: bool = false,
+  status: HttpCode = Http406,
   error: JsonNode = %*{},
   data: JsonNode = %*{}): ApiMsg =
-  result = ApiMsg(success: success, error: error, data: data)
+  result = ApiMsg(
+    status: status,
+    success: success,
+    error: error,
+    data: data)
